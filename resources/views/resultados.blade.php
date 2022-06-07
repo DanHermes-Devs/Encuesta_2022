9fc59f2162d5761997cb398bf115ba1aa50bcedb
Dan Hermes <80308086+DanHermes-Devs@users.noreply.github.com>
2022-06-07 10:48:16 -0500

    Update project 6

--- resources/views/resultados.blade.php
+++ resources/views/resultados.blade.php
@@ -19,8 +19,11 @@
     <div class="container bg-white mt-5 p-5">
         <div class="row justify-content-center">
             @php
-                $cookieGlobal = $_COOKIE['cookieCalificaciones'];
-                $registroID = App\Registro::where('token', $cookieGlobal)->first();
+                // $cookieGlobal = $_COOKIE['cookieCalificaciones'];
+                // $registroID = App\Registro::where('token', $cookieGlobal)->first();
+                $registroID = App\Registro::where('id', 13)->first();
+                $Cfinal = App\Registro::where('id', 13)->first();
+                $calificacionFinal = $Cfinal->item_1+$Cfinal->item_2+$Cfinal->item_3+$Cfinal->item_4+$Cfinal->item_5+$Cfinal->item_6+$Cfinal->item_7+$Cfinal->item_8+$Cfinal->item_9+$Cfinal->item_10+$Cfinal->item_11+$Cfinal->item_12+$Cfinal->item_13+$Cfinal->item_14+$Cfinal->item_15+$Cfinal->item_16+$Cfinal->item_17+$Cfinal->item_18+$Cfinal->item_19+$Cfinal->item_20+$Cfinal->item_21+$Cfinal->item_22+$Cfinal->item_23+$Cfinal->item_24+$Cfinal->item_25+$Cfinal->item_26+$Cfinal->item_27+$Cfinal->item_28+$Cfinal->item_29+$Cfinal->item_30+$Cfinal->item_31+$Cfinal->item_32+$Cfinal->item_33+$Cfinal->item_34+$Cfinal->item_35+$Cfinal->item_36+$Cfinal->item_37+$Cfinal->item_38+$Cfinal->item_39+$Cfinal->item_40+$Cfinal->item_41+$Cfinal->item_42+$Cfinal->item_43+$Cfinal->item_44+$Cfinal->item_45+$Cfinal->item_46+$Cfinal->item_47+$Cfinal->item_48+$Cfinal->item_49+$Cfinal->item_50+$Cfinal->item_51+$Cfinal->item_52+$Cfinal->item_53+$Cfinal->item_54+$Cfinal->item_55+$Cfinal->item_56+$Cfinal->item_57+$Cfinal->item_58+$Cfinal->item_59+$Cfinal->item_60+$Cfinal->item_61+$Cfinal->item_62+$Cfinal->item_63+$Cfinal->item_64+$Cfinal->item_65+$Cfinal->item_66+$Cfinal->item_67+$Cfinal->item_68+$Cfinal->item_69+$Cfinal->item_70+$Cfinal->item_71+$Cfinal->item_72;
             @endphp
             <div class="col-12 mb-5">
                 <h2>Resultados</h2>
@@ -102,6 +105,37 @@
                     {{ $registroID->tipo_contratacion }}
                 </p>
             </div>
+        </div>
+
+        <div class="row mt-3">
+            <table class="table table-stripped table-bordered">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Calificación</th>
+                        <th scope="col">Resultado</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr class="text-center">
+                        <th scope="row">1</th>
+                        <td scope="row">Calificación final del cuestionario</td>
+                        <td scope="row">
+                            @if ($calificacionFinal < '50')
+                                <span class="badge badge-success">{{ $calificacionFinal }}</span>
+                            @elseif ($calificacionFinal <= '50' || $calificacionFinal < '75')
+                                <span class="badge badge-success-low">{{ $calificacionFinal }}</span>
+                            @elseif ($calificacionFinal <= '75' || $calificacionFinal < '99')
+                                <span class="badge badge-warning">{{ $calificacionFinal }}</span>
+                            @elseif ($calificacionFinal <= '99' || $calificacionFinal < '140')
+                                <span class="badge badge-danger">{{ $calificacionFinal }}</span>
+                            @elseif ($calificacionFinal >= '140')
+                                
+                            @endif
+                        </td>
+                    </tr>
+            </table>
         </div>
     </div>
 @endsection
