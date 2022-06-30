d12f2f1e80c31b1bc3e6cf9ae103964e52cb00c3
xdhermes <80308086+DanHermes-Devs@users.noreply.github.com>
2022-06-30 11:32:23 -0500

    Cambios en botones de accion de empresa

--- resources/views/admin/empresas/actions.blade.php
+++ resources/views/admin/empresas/actions.blade.php
@@ -1,8 +1,10 @@
 {{-- Crear botones para insertar en datatables --}}
 <a href="/empresa/{{ $token }}" target="_blank" title="Ver encuesta" class="btn btn-success"><i class="fas fa-eye"></i></a>
 
+<a href="{{ route("empresas.getGraficas", $id) }}" data-toggle="tooltip" title="Ver datos generales" class="btn btn-info"><i class="fas fa-clipboard-list"></i></a>
+
 <a href="{{ route("empresas.edit", $id) }}" data-toggle="tooltip" title="Editar empresa" class="btn btn-warning"><i class="fa fa-pencil-alt"></i></a>
 
-<a href="{{ route("empresas.getGraficas", $id) }}" data-toggle="tooltip" title="Ver graficas" class="btn btn-info"><i class="fas fa-chart-pie"></i></a>
+{{-- <a href="{{ route("empresas.getGraficas", $id) }}" data-toggle="tooltip" title="Ver graficas" class="btn btn-info"><i class="fas fa-chart-pie"></i></a> --}}
 
 <button type="button" name="delete" data-id="{{ $id }}" title="Eliminar empresa" class="delete btn btn-danger"><i class="fas fa-trash-alt"></i></button>
